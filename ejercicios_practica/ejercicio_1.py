2351583eda2a6dcd0af68f86dbec05f52c7c29fb
Abel Garzón <abelgarzon@gmail.com>
2022-11-09 22:59:21 -0500

    python_inicial_class_5_funciones

---
+++ ejercicios_practica/ejercicio_1.py
@@ -0,0 +1,31 @@
+# Funciones [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.2
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con funciones
+
+def imprimir_mayor(numero_1, numero_2):
+    print("Funcion imprimir mayor")
+    # En esta función debe determinar cual de los dos
+    # números ingresados por parámetro es mayor
+    # y luego imprimir dicho valor en pantalla
+    if numero_1 == numero_2:
+        print('Los números ingresados son eguales')
+    elif numero_1 < numero_2:
+        print(f'el número {numero_2} es mayor a el número {numero_1}')
+    else:
+        print(f'el número {numero_1} es mayor a el número {numero_2}')
+
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+
+    # Alumno: Complete la función "imprimir_mayor"
+    imprimir_mayor(2, 10)
+
+    print("terminamos")
